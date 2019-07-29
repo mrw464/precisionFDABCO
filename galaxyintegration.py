64bd4b29a00cd86bc1644d95a03d23875be069b6
mrw464 <mwisinger@deloitte.com>
2019-07-29 14:05:45 -0400

    initial set up of the galaxy integration

--- galaxyintegration.py
+++ galaxyintegration.py
@@ -1,1 +1,15 @@
+from bioblend.galaxy import GalaxyInstance
 
+
+galaxyIP = str(input('What is the URL address of the Galaxy instance?\n'))
+galaxyAPIkey = str(input('What is the API key of the Galaxy instance?\n'))
+
+
+gi = GalaxyInstance(galaxyIP, key=galaxyAPIkey)
+libs = gi.libraries.get_libraries()
+
+workflowID = str(input('What is the workflow ID to import?\n'))
+
+gi.workflows.show_workflow(workflowID)
+
+#gi.workflows.run_workflow('workflow ID', input_dataset_map)

02a74ce0d4debdacc561233d9074bfb7aa82b197
mrw464 <mwisinger@deloitte.com>
2019-07-29 16:18:43 -0400

    can import workflow and write to JSON (for testing) need to review the JSON output to understand what is contained Won't be a 1:1 from workfrom to BCO format

--- galaxyintegration.py
+++ galaxyintegration.py
@@ -1,5 +1,5 @@
 from bioblend.galaxy import GalaxyInstance
-
+import json
 
 galaxyIP = str(input('What is the URL address of the Galaxy instance?\n'))
 galaxyAPIkey = str(input('What is the API key of the Galaxy instance?\n'))
@@ -12,4 +12,17 @@ workflowID = str(input('What is the workflow ID to import?\n'))
 
 gi.workflows.show_workflow(workflowID)
 
+workflowDict = gi.workflows.export_workflow_dict(workflowID)
+
+
+with open('test_workflow.json', 'w', encoding='utf-8') as f:
+    json.dump(workflowDict, f, ensure_ascii=False, indent=4)
+
+
+
+
+
+# test commands
+#gi.workflows.export_workflow_to_local_path(workflowID,r"C:\Users\mwisinger\Documents\precisionFDA BCO\githubrepo",
+#   use_default_filename=True)
 #gi.workflows.run_workflow('workflow ID', input_dataset_map)
